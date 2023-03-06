0598115c0e1beed9ef13a6285119e953eb1a87e6
NI-KI54 <nickmasih575@gmail.com>
2023-03-06 20:51:56 +0530

    added 3 variables added a function called the function

---
+++ main.js
@@ -0,0 +1,9 @@
+const hourEL = document.querySelector(".hour");
+const minuteEL = document.querySelector(".minute");
+const secondEL = document.querySelector(".second");
+
+function updateClock(){
+
+}
+
+updateClock();
